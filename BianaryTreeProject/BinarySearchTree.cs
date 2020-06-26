1447e13628b9107f2d2cda05c6ba5d8bad47106f
unknown <adamjsteinbacher@gmail.com>
2020-06-26 14:33:51 -0500

    created classes Node/BinarySearchTree/ Started Add method / created member variables

---
+++ BianaryTreeProject/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BianaryTreeProject
+{
+    public class BinarySearchTree
+    {
+
+        public Node rootNode;
+
+
+        
+        public void Add(int value)
+        {
+            Node node = new Node();
+            node.value = value;
+
+            if(rootNode == null)
+            {
+                rootNode = node;
+            }
+            else
+            {
+
+            }
+
+
+            //create new node
+            //check if rootNode is null. If so, make initial node the root
+            //
+
+
+        }
+
+
+        public void Search()
+        {
+
+
+
+
+
+
+        }
+
+
+
+    }   
+    
+    
+    
+}
---
+++ BianaryTreeProject/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BianaryTreeProject
+{
+    public class Node
+    {
+        public int value;
+        public Node leftNode;
+        public Node RightNode;
+    }
+}
